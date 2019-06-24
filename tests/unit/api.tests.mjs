7b373603af56b6be24cca25a917040ec6e685bdd
KARTHIKEYAN DAKSHINAMURTHY <karthid@hcl.com>
2019-06-24 10:28:45 +0530

    refactor reducers into their own classes #3

--- tests/unit/api.tests.mjs
+++ tests/unit/api.tests.mjs
@@ -1,23 +1,24 @@
 import test from 'tape'
-import sudoku from '../../lib/sudoku'
+import Reducer from '../../lib/reducers/Reducer'
+const baseReducer = new Reducer()
 
 test('getBoxXY', (t) => {
-    let [x,y] = sudoku().getBoxXY(0)
+    let [x,y] = baseReducer.getBoxXY(0)
     t.deepEquals([x,y], [0,0]);
 
-    [x,y] = sudoku().getBoxXY(1)
+    [x,y] = baseReducer.getBoxXY(1)
     t.deepEquals([x,y], [0,3]);
 
-    [x,y] = sudoku().getBoxXY(2)
+    [x,y] = baseReducer.getBoxXY(2)
     t.deepEquals([x,y], [0,6]);
 
-    [x,y] = sudoku().getBoxXY(3)
+    [x,y] = baseReducer.getBoxXY(3)
     t.deepEquals([x,y], [3,0]);
 
-    [x,y] = sudoku().getBoxXY(8)
+    [x,y] = baseReducer.getBoxXY(8)
     t.deepEquals([x,y], [6,6]);
 
-    [x,y] = sudoku().getBoxXY(10)
+    [x,y] = baseReducer.getBoxXY(10)
     t.deepEquals([x,y], [6,6]);
 
     t.end()
